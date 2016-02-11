b55e0d9432362fec9ccdf14a07ab1f1567090dec
Joel Hermanns <joel.hermanns@gmail.com>
2016-02-11 16:16:58 +0100

    Add some more boilerplate

--- quali/src/Main.hs
+++ quali/src/Main.hs
@@ -1,5 +1,36 @@
 module Main where
 
+
+import System.Environment
+
+data Input = Input deriving Show
+
+data Output = Output deriving Show
+
+calc :: Input -> IO Output
+calc input = undefined
+
+parseInput :: String -> Input
+parseInput str = Input
+  where
+    (l:ls) = lines str
+    (x:xs) = words l
+    rest = map id ls
+
+outputToString :: Output -> String
+outputToString out = show out
+
+fromFile :: FilePath -> IO Input
+fromFile fp = do
+  content <- readFile fp
+  return $ parseInput content
+
+toFile :: FilePath -> Output -> IO ()
+toFile fp = writeFile fp . outputToString
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  (title:_) <- getArgs
+  input <- fromFile (title ++ ".in")
+  output <- calc input
+  toFile (title ++ ".out") output
